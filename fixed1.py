42cbd2647d232c43f99d2a6685a4e2cc983f97e8
Raghuvendra <raghuvendra957@gmail.com>
2026-07-12 17:30:32 +0530

    third bug fixed

---
+++ fixed1.py
@@ -0,0 +1,12 @@
+# fixed code of 1.py
+x = 102
+if x > 2:
+    print("x is greater than 2")
+elif x > 1:
+    print("x is greater than 1")
+else:
+    print("x is not greater than 1")
+
+print("programming done")
+print("program ends here")
+print("one more bug detected and fixed")
