2723ceb6a2504da91fa8c50d07da97bd4c5aeab3
Raghuvendra <raghuvendra957@gmail.com>
2026-07-12 23:24:42 +0530

    added new file

---
+++ 70.py
@@ -0,0 +1,6 @@
+# wwrite a code for calculating odd numbers from 1-100 and get them in the list
+
+odd_list = [i for i in range(1, 100) if i % 2 != 0]
+print(odd_list)
+
+print("program ended here")
