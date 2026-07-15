f6af69ec46c0c8b1eecddcc3fbf65aa14a096095
Raghuvendra <raghuvendra957@gmail.com>
2026-07-15 23:21:26 +0530

    added 78.py

---
+++ 78.py
@@ -0,0 +1,19 @@
+import os
+
+# get the current working directory
+#print(os.getcwd())
+
+#print("-----------------")
+
+# create the folder
+
+#os.mkdir("my_file")
+#print("folder created")
+#print("------------------")
+
+# delete the folder
+
+os.rmdir("my_file")
+print("folder deleted")
+print("------------------")
+
