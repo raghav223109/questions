f1449908a6ac01fcab3cff80fe957250a418d4c6
Raghuvendra <raghuvendra957@gmail.com>
2026-07-21 22:19:25 +0530

    added 112.py

---
+++ 112.py
@@ -0,0 +1,11 @@
+# create a list containing the sqaures of all the elements in the list 
+
+num = [1,2,3,4,5,6,7,8,9,10]
+
+num2 = []
+
+for i in num:
+    sq = i**2
+    num2.append(sq)
+
+print(num2)
