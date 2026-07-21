aaeb3a23cea2281103bf438251eeaf7dc105eeaa
Raghuvendra <raghuvendra957@gmail.com>
2026-07-21 13:40:12 +0530

    added 91.py

---
+++ 91.py
@@ -0,0 +1,15 @@
+# deleting front digit from the number and print only the back digit with out string slicing
+
+n = 123
+b = n%10
+print(b)
+
+m = 432
+p = m//10
+print(p)
+
+o = 32
+a = o * 10 + 1
+y = a * 10 + 0
+print(a)
+print(y)
