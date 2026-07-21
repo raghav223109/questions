ccfd47e24af260ed00e87e5d256b53a0424bee16
Raghuvendra <raghuvendra957@gmail.com>
2026-07-21 15:06:47 +0530

    added 94.py

---
+++ 94.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+
+for num in range (a ,b+1):
+    num_str = str(num)
+    n = len(num_str)
+    result = 0
+
+    for digit_char in num_str:
+        digit = int(digit_char)
+        result = result + digit**n
+
+    if result == num:
+        print(num)
